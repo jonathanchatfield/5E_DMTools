793149b41bb21434b84ba8d54be565ef867d8a87
JonathanChatfield <jonathan.chatfield@outlook.com>
2022-07-28 17:04:08 -0400

    updated basic_stats generator

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import random_generators
 
-for i in range(1, 1000):
-    print(f"Your NPC is a/an: {random_generators.random_race()} {random_generators.random_class()}")
-    print(f"Their stats are {random_generators.basic_stats()}")
+print(f"Your NPC is a/an: {random_generators.random_race()} {random_generators.random_class()}")
+print(f"Their stats are {random_generators.basic_stats()}")
