f1b74af0c1a9e279bfe0b73a4780d6be4e9972c0
JonathanChatfield <jonathan.chatfield@outlook.com>
2022-07-28 19:15:41 -0400

    updated basic_stats_1e

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import random_generators
 
 print(f"Your NPC is a/an: {random_generators.random_race()} {random_generators.random_class()}")
 print(f"Their stats are {random_generators.basic_stats()}")
-print(f"Their stats are {random_generators.basic_stats_1e()}")
 
+# 1st Edition Ruleset
+# stats = random_generators.basic_stats_1e()
+# print("Their stats are:")
+# for i, j in stats:
+#     print(i, j)
+
+# todo set up minimum stats for classes 
