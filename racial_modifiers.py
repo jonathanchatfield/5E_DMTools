dea6b45cdc91778c394335a378715ca1d54107c3
JonathanChatfield <jonathan.chatfield@outlook.com>
2022-08-01 21:40:46 -0400

    updates and refactors

--- racial_modifiers.py
+++ racial_modifiers.py
@@ -1,56 +1,164 @@
-dragonborn = {'Strength': 2,
+dwarf_hill = {'Strength': 0,
               'Dexterity': 0,
-              'Constitution': 0,
+              'Constitution': 2,
               'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+              'Wisdom': 1,
+              'Charisma': 0}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+dwarf_mountain = {'Strength': 2,
+                  'Dexterity': 0,
+                  'Constitution': 2,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+elf_high = {'Strength': 0,
+            'Dexterity': 2,
+            'Constitution': 0,
+            'Intelligence': 1,
+            'Wisdom': 0,
+            'Charisma': 0}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+elf_wood = {'Strength': 0,
+            'Dexterity': 2,
+            'Constitution': 0,
+            'Intelligence': 0,
+            'Wisdom': 1,
+            'Charisma': 0}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+elf_drow = {'Strength': 0,
+            'Dexterity': 2,
+            'Constitution': 0,
+            'Intelligence': 0,
+            'Wisdom': 0,
+            'Charisma': 1}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+halfling_lightfoot = {'Strength': 0,
+                      'Dexterity': 2,
+                      'Constitution': 0,
+                      'Intelligence': 0,
+                      'Wisdom': 0,
+                      'Charisma': 1}
 
-dragonborn = {'Strength': 2,
-              'Dexterity': 0,
-              'Constitution': 0,
-              'Intelligence': 0,
-              'Wisdom': 0,
-              'Charisma': 1}
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
               
+human = {'Strength': 1,
+         'Dexterity': 1,
+         'Constitution': 1,
+         'Intelligence': 1,
+         'Wisdom': 1,
+         'Charisma': 1}
+
 dragonborn = {'Strength': 2,
               'Dexterity': 0,
               'Constitution': 0,
               'Intelligence': 0,
               'Wisdom': 0,
               'Charisma': 1}
+
+gnome_forest = {'Strength': 0,
+                'Dexterity': 1,
+                'Constitution': 0,
+                'Intelligence': 2,
+                'Wisdom': 0,
+                'Charisma': 0}
+
+gnome_rock = {'Strength': 0,
+              'Dexterity': 0,
+              'Constitution': 1,
+              'Intelligence': 2,
+              'Wisdom': 0,
+              'Charisma': 0}
+
+half_elf = {'Strength': 0,
+            'Dexterity': 1,
+            'Constitution': 1,
+            'Intelligence': 0,
+            'Wisdom': 0,
+            'Charisma': 2}
+
+half_orc = {'Strength': 2,
+            'Dexterity': 0,
+            'Constitution': 1,
+            'Intelligence': 0,
+            'Wisdom': 0,
+            'Charisma': 0}
+
+tiefling = {'Strength': 0,
+            'Dexterity': 0,
+            'Constitution': 1,
+            'Intelligence': 0,
+            'Wisdom': 0,
+            'Charisma': 2}
+
+aasimar = {'Strength': 0,
+           'Dexterity': 0,
+           'Constitution': 0,
+           'Intelligence': 0,
+           'Wisdom': 1,
+           'Charisma': 2}
+
+##left off here
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
+
+
+halfling_stout = {'Strength': 0,
+                  'Dexterity': 2,
+                  'Constitution': 1,
+                  'Intelligence': 0,
+                  'Wisdom': 0,
+                  'Charisma': 0}
 
